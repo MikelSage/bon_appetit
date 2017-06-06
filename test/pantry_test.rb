9fff2505b4cf6286cb28c849c902266d214e60cf
MikelSage <michael.sagapolutele@yahoo.com>
2017-06-06 09:10:21 -0600

    create pantry class

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,12 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  attr_reader :pantry
+  def setup
+    @pantry = Pantry.new
+  end
 
+  def test_it_exists
+    assert_instance_of Pantry, pantry
+  end
 end
