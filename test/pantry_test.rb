a86d371dc8f0e95f70c326deebd19ffecf8b2434
MikelSage <michael.sagapolutele@yahoo.com>
2017-06-06 09:14:16 -0600

    add initialize method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -11,4 +11,13 @@ class PantryTest < Minitest::Test
   def test_it_exists
     assert_instance_of Pantry, pantry
   end
+
+  def test_it_can_check_its_stock_and_it_starts_empty
+    assert_instance_of Hash, pantry.stock
+    assert pantry.stock.empty?
+  end
+
+  def test_it_can_check_stock_of_item
+    
+  end
 end
